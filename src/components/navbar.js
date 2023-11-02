16426debbb4ee153828f187f52029632f44028ed
Deru <deru.yang0086@gmail.com>
2023-11-02 20:46:55 +0800

    fix: Adjust navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,11 +1,12 @@
-import React, { useState, useEffect } from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '@fortawesome/fontawesome-svg-core/styles.css';
+import React from "react";
 import { Navbar, Nav, Container, Button } from 'react-bootstrap';
 import { useTranslation } from 'next-i18next';
 import { getCookie, setCookie } from "cookies-next";
 import getConfig from "next/config";
 import i18n from '../utils/i18n';
 import styles from '../styles/navbar.module.css';
-import '@fortawesome/fontawesome-svg-core/styles.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEarthAmericas } from '@fortawesome/free-solid-svg-icons';
 
@@ -31,22 +32,22 @@ const NavBar = () => {
 
 	return (
 		<>
-		<Navbar collapseOnSelect expand='lg' bg='cream' className='m-4'>
-			<Container className='flex flex-row justify-between'>
-				<Navbar.Brand href='/' className='text-2xl text-dark-blue'>{ t('Stadium Matching System') }</Navbar.Brand>
-				<Navbar.Toggle/>
-				<Navbar.Collapse>
-					<Nav className='me-auto'></Nav>
-					<Nav className='d-flex align-items-center'>
-						<Nav.Link href='/' className={styles.navLink}>{ t('Home') }</Nav.Link>
-						<Nav.Link href='/main/venue' className={styles.navLink}>{ t('Venue') }</Nav.Link>
-						<Nav.Link href='/' className={styles.navLink}>{ t('Login') }</Nav.Link>
-						<Button 
+		<Navbar collapseOnSelect expand="sm" bg="cream">
+			<Container className="m-2">
+				<Navbar.Brand href="/" bsPrefix="text-2xl no-underline" className="text-dark-blue">{ t('Stadium Matching System') }</Navbar.Brand>
+				<Navbar.Toggle aria-controls="responsive-navbar-nav"/>
+				<Navbar.Collapse id="responsive-navbar-nav">
+					<Nav className="me-auto"></Nav>
+					<Nav>
+						<Nav.Link href="/" className={styles.navLink}>{ t('Home') }</Nav.Link>
+						<Nav.Link href="/main/venue" className={styles.navLink}>{ t('Venue') }</Nav.Link>
+						<Nav.Link href="/" className={styles.navLink}>{ t('Login') }</Nav.Link>
+						<Nav.Link 
 							className={styles.navLink}
 							onClick={handleLanguage}
 						>
 							<FontAwesomeIcon icon={faEarthAmericas} className='mr-2 flex flex-row'/>{'中 | EN'}
-						</Button>
+						</Nav.Link>
 					</Nav>
 				</Navbar.Collapse>
 			</Container>
--- src/pages/main/venue.js
+++ src/pages/main/venue.js
@@ -12,14 +12,14 @@ const Venue = () => {
 			<Head>
 				<title>{ t('Stadium Matching System') }</title>
 				<meta
-					property='og:description'
-					content='Stadium Matching System'
+					property="og:description"
+					content="Stadium Matching System"
 				/>
 			</Head>
 			<NavBar/>
 			<Container>
 				<Row>
-					<Col className='text-center text-3xl'>
+					<Col className="text-center text-3xl">
 						<h1>{ t('Venue') }</h1>
 					</Col>
 				</Row>
