91d334df0bf02a9647ca39ac4e21fe4b23ea9fa9
musicat1020 <musicat1020@gmail.com>
2023-11-07 22:56:31 +0800

    feat: add record page

--- mockData/mockData.js
+++ mockData/mockData.js
@@ -30,3 +30,68 @@ export const mockVenueList = [
         userMax: 604,
     }
 ]
+
+export const mockRentalRecords = [{
+    date: "2021-10-11",
+    time: "10:00~11:00",
+    stadium: "綜合體育館",
+    venue: "一樓多功能球場 A場",
+    status: "已核准",
+    numOfPeople: "3/6",
+    renter: '許姍姍',
+    members: ["王曉明"]
+},
+{
+    date: "2021-10-05",
+    time: "10:00~11:00",
+    stadium: "綜合體育館",
+    venue: "一樓多功能球場 A場",
+    status: "已核准",
+    numOfPeople: "4/6",
+    renter: '許姍姍',
+    members: ["王曉明", "吳小華", "陳大明"]
+},
+{
+    date: "2021-10-01",
+    time: "10:00~11:00",
+    stadium: "綜合體育館",
+    venue: "一樓多功能球場 A場",
+    status: "已取消",
+    numOfPeople: "4/6",
+    renter: '許姍姍',
+    members: ["王曉明", "吳小華", "陳大明"]
+}
+]
+
+
+export const mockPairingRecords = [{
+    date: "2021-10-11",
+    time: "10:00~11:00",
+    stadium: "綜合體育館",
+    venue: "一樓多功能球場 A場",
+    status: "已加入",
+    numOfPeople: "3/6",
+    renter: '許姍姍',
+    members: ["王曉明"]
+},
+{
+    date: "2021-10-05",
+    time: "10:00~11:00",
+    stadium: "綜合體育館",
+    venue: "一樓多功能球場 A場",
+    status: "已取消",
+    numOfPeople: "4/6",
+    renter: '許姍姍',
+    members: ["王曉明", "吳小華", "陳大明"]
+},
+{
+    date: "2021-10-01",
+    time: "10:00~11:00",
+    stadium: "綜合體育館",
+    venue: "一樓多功能球場 A場",
+    status: "已退出",
+    numOfPeople: "4/6",
+    renter: '許姍姍',
+    members: ["王曉明", "吳小華", "陳大明"]
+}
+]
---
+++ src/components/recordTable.js
@@ -0,0 +1,63 @@
+
+import styles from '../styles/record.module.css';
+import { useTranslation } from 'next-i18next';
+import { Button } from 'react-bootstrap';
+
+const RecordTable = ({ records }) => {
+    const { t } = useTranslation();
+
+    return (
+
+
+        <div className="rounded-xl  overflow-hidden border border-cream">
+            <table className="table-auto" style={{ width: "100%" }}>
+                <thead>
+                    <tr className="bg-cream justify-center items-center">
+                        <th className="px-4 py-2 text-center">{t('Rental Period')}</th>
+                        <th className="px-4 py-2 text-center">{t('Venue')}</th>
+                        <th className="px-4 py-2 text-center">{t('Order Status')}</th>
+                        <th className="px-4 py-2 text-center">{t('Number of People')}</th>
+                        <th className="px-4 py-2 text-center">{t('Members')}</th>
+                        <th className="px-4 py-2 text-center"></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {records.map((record, idx) => {
+                        const isCancelled = record.status == "已取消" ? true : false;
+                        return (
+                            <tr key={idx} className="border-t border-cream ">
+                                <td className={`x-4 py-2 ${isCancelled ? 'text-gray' : ''}`}>
+                                    <div className="flex flex-col justify-center items-center">
+                                        <text>{record.date}</text>
+                                        <text className='text-xs'>{record.time}</text>
+                                    </div>
+                                </td>
+
+                                <td className={`px-4 py-2 ${isCancelled ? 'text-gray' : ''}`}>
+                                    <div className="flex flex-col justify-center items-center">
+                                        <text>{record.stadium}</text>
+                                        <text className='text-xs'>{record.venue}</text>
+                                    </div>
+                                </td>
+
+                                <td className={`px-4 py-2 text-center ${isCancelled ? 'text-gray' : ''}`}>{record.status}</td>
+                                <td className={`px-4 py-2 text-center ${isCancelled ? 'text-gray' : ''}`}>{record.numOfPeople}</td>
+                                <td className={`px-4 py-2 text-center ${isCancelled ? 'text-gray' : ''}`}>{record.members.join(", ")}</td>
+                                <td className={`px-4 py-2 text-center ${isCancelled ? 'text-gray' : ''}`}>
+                                    <Button
+                                        className={styles.cancelButton}
+                                        disabled={isCancelled}
+                                    >
+                                        {t('Cancel Rental')}
+                                    </Button>
+                                </td>
+                            </tr>
+                        );
+                    })}
+                </tbody>
+            </table>
+        </div >
+    );
+};
+
+export default RecordTable;
