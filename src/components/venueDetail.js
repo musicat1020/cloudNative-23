7440fc89e1d301096bd910b7a86b2d642fc00443
juliawupei <juliawu0717@gmail..com>
2023-11-07 23:07:45 +0800

    feat: add venue detail tab

---
+++ src/components/venueDetail.js
@@ -0,0 +1,16 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '@fortawesome/fontawesome-svg-core/styles.css';
+import React from "react";
+import Box from '@mui/material/Box';
+import { Navbar, Nav, Container, Button } from 'react-bootstrap';
+
+const VenueDetail = () => {
+  return (
+    <>
+
+
+    </>
+  );
+}
+
+export default VenueDetail;
