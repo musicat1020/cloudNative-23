db437653eeec5a783a885bf7303874f2c9a69201
juliawupei <juliawu0717@gmail..com>
2023-11-16 22:43:18 +0800

    feat: newVenue/editVenue/deleteVenue/deleteSessionVenue modal

---
+++ src/components/deleteVenueSessionModal.js
@@ -0,0 +1,124 @@
+import { Container, Row, Col } from "react-bootstrap";
+import { useTranslation } from "react-i18next";
+import { makeStyles } from "@mui/styles";
+import Modal from "@mui/material/Modal";
+import Divider from "@mui/material/Divider";
+import styles from "../styles/modal.module.css";
+import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import { TimePicker } from '@mui/x-date-pickers/TimePicker';
+
+const useStyles = makeStyles({
+  modal: {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    boxShadow: 24,
+    width: "50vw",
+    padding: "15px 30px",
+    backgroundColor: "white",
+    color: "#14274C",
+    outline: 0,
+    borderRadius: "5px",
+  }
+});
+
+function DeleteVenueSessionModal({ show, handleClose, title, info, customStyles }) {
+
+  const modalStyles = useStyles();
+  const { t } = useTranslation();
+
+  // TODO
+  const handleConfirm = () => {
+    handleClose();
+  }
+
+  return (
+    <>
+      {show &&
+        <Modal
+          open={show}
+          onClose={handleClose}
+          aria-labelledby='modal-modal-title'
+          aria-describedby='modal-modal-description'
+        >
+          <Container className={modalStyles.modal} style={customStyles}>
+            {/** Close button */}
+            <Row>
+              <Col className='text-end text-3xl'>
+                <button onClick={handleClose}>&times;</button>
+              </Col>
+            </Row>
+
+            {/** Title */}
+            <Row>
+              <Col className='text-center'>
+                <h1>{title}</h1>
+              </Col>
+            </Row>
+
+            {/** stadium name */}
+            <Row className='mt-3'>
+              <Col>
+                <span className={styles.modalAttribute}>{t("場館名稱")}</span>
+                <span>{info?.stadium?.stadium_name}</span>
+              </Col>
+            </Row>
+
+            {/** venue name */}
+            <Row className='mt-3'>
+              <Col>
+                <span className={styles.modalAttribute}>{t("場地名稱")}</span>
+                <span>{info?.stadium?.name}</span>
+              </Col>
+            </Row>
+
+            {/** Divider */}
+            <Row className='my-3'>
+              <Col>
+                <Divider />
+              </Col>
+            </Row>
+
+            {/** start date/time */}
+            <Row className='my-3'>
+              <Col className="ml-8">
+                <div className="mb-3">{t("開始日期/時間")}</div>
+                <LocalizationProvider dateAdapter={AdapterDayjs}>
+                  <DatePicker className="mr-5" label="date" />
+                  <TimePicker views={['hours']} />
+                </LocalizationProvider>
+              </Col>
+            </Row>
+
+            {/** end date/time */}
+            <Row className='my-4'>
+              <Col className="ml-8">
+                <div className="mb-3">{t("結束日期/時間")}</div>
+                <LocalizationProvider dateAdapter={AdapterDayjs}>
+                  <DatePicker className="mr-5" label="date" />
+                  <TimePicker views={['hours']} />
+                </LocalizationProvider>
+              </Col>
+            </Row>
+
+            {/* Button */}
+            <Row className='mt-5'>
+              <Col className='text-center' >
+                <button className={styles.confirmButton} onClick={handleClose}>{t("取消")}</button>
+              </Col>
+              <Col className='text-center'>
+                <button className={styles.confirmButton} onClick={handleConfirm}>{t("確定")}</button>
+              </Col>
+            </Row>
+          </Container>
+        </Modal>
+      }
+    </>
+  );
+}
+
+export default DeleteVenueSessionModal;
--- src/pages/admin/editVenue.js
+++ src/pages/admin/editVenue.js
@@ -8,32 +8,29 @@ import { useEffect, useState } from "react";
 import NavBar from "../../components/navbarAdmin";
 import VenueDetail from "../../components/venueDetail";
 import styles from "../../styles/venue.module.css";
-import { mockVenueList } from "../../../mockData/mockData";
+import axios from "../../utils/axios";
+import { mockVenueList, mockVenueDetail } from "../../../mockData/mockData";
+import EditVenueModal from "@/components/editVenueModal";
 // import Image from 'react-bootstrap/Image';
 
-function Venue() {
+function EditVenue() {
 
 	const router = useRouter();
 	// the index is assigned the value of router.query.venue
 	const { venue: index } = router.query;
-	const [venueInfo, setVenueInfo] = useState();
+	const [venueInfo, setVenueInfo] = useState(mockVenueDetail[0]);
+	const [showEditVenueModal, setShowEditVenueModal] = useState(false);
 	const { t } = useTranslation();
 
-	useEffect(() => {
-		const fetchVenues = async () => {
-			try {
-				// TODO:
-				// const response = await fetch('/stadium/list');
-				// const data = await response.json();
-				// setVenues(data);
-				const data = mockVenueList[index - 1];
-				setVenueInfo(data);
-			} catch (error) {
-				throw new Error(error);
-			}
+	const fetchVenueDetail = async (id) => {
+		const params = {
+			stadium_id: 1,
 		};
-		fetchVenues();
-	}, []);
+		const res = await axios.post("/api/v1/stadium/info", {}, { params });
+		console.log(res.data);
+	};
+	fetchVenueDetail();	
+	
 
 	const theme = createTheme({
 		palette: {
@@ -70,28 +67,46 @@ function Venue() {
 				<div className="flex justify-center items-center">
 					<img
 						className='rounded-lg object-cover w-5/6 h-96 hover:opacity-75'
-						src={venueInfo?.imgUrl}
+						src={venueInfo?.stadium?.imgUrl}
 						alt="Picture of the venue"
 					/>
 				</div>
 
 				<Row>
 					<Col xs={{ span: 10, offset: 1 }}>
-						<VenueDetail />
+						<VenueDetail info={venueInfo} setInfo={setVenueInfo} />
 					</Col>
 				</Row>
 
 				<Row className='flex'>
 					<Col className="text-center ">
 						<div spacing={2} direction="row" className='space-x-7'>
-							<Button variant="outlined" color="secondary">{t("修改")}</Button>
-							<Button variant="outlined" color="secondary">{t("取消")}</Button>
+							<Button
+								variant="outlined"
+								color="secondary"
+								onClick={() => setShowEditVenueModal(true)}
+							>
+								{t("修改")}
+							</Button>
+							<Button
+								variant="outlined"
+								color="secondary"
+							>
+								{t("取消")}
+							</Button>
 						</div>
 					</Col>
 				</Row>
 			</Container>
+			<EditVenueModal
+				show={showEditVenueModal}
+				setShow={setShowEditVenueModal}
+				handleClose={() => setShowEditVenueModal(false)}
+				title={t("新增場地")}
+				info={venueInfo} />
+
 		</ThemeProvider>
 	);
 }
 
-export default Venue;
+export default EditVenue;
