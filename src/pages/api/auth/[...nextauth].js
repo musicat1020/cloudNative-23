06a15ed5baacb90bd74f0e093806b5ec1275c888
musicat1020 <musicat1020@gmail.com>
2023-11-18 21:16:52 +0800

    feat: get accessToken from backend

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,6 +1,7 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import jwt from "jsonwebtoken";
+import axios from "axios";
 
 export const authOptions = {
 
@@ -15,28 +16,12 @@ export const authOptions = {
     },
     // ref:https://stackoverflow.com/questions/76905467/how-to-access-token-in-next-auth
     callbacks: {
-        session: async ({ session, token }) => {
-            const accessToken = jwt.sign(
-                {
-                    name: token.name,
-                    email: token.email,
-                    picture: token.picture,
-                    sub: token.sub,
-                },
-                process.env.JWT_SECRET, // JWT_SECRET is a secret string that only the server knows and should be regularly updated
-                {
-                    expiresIn: process.env.JWT_EXPIRES_IN, // JWT_EXPIRES_IN is the duration of the token
-                },
-            );
+        async session({ session, token }) {
             session.user = token;
-            session.accessToken = accessToken;
-            // console.log("session", session);
             return session;
         },
     }
-
 };
-
 
 export default NextAuth(authOptions);
 
